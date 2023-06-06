554cf2a2fcdf774206d3578347eb18000ae10c41
AnshSaxena05 <114876476+AnshSaxena05@users.noreply.github.com>
2023-06-06 15:13:41 +0530

    Add files via upload

---
+++ removeElement_7.java
@@ -0,0 +1,14 @@
+public class removeElement_7 
+{
+    public int removeElement(int[] nums, int val) 
+    {
+        int k = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != val) {
+                nums[k] = nums[i];
+                k++;
+            }
+        }
+        return k;
+    }
+}
